7bdbd712a89e23a0d39d2747ee46cb19f83f63f5
PhantomZoro <phaneendrab.iitm@outlook.com>
2024-12-24 17:55:29 +0530

    functional components

--- App.js
+++ App.js
@@ -1,15 +1,39 @@
-const parent = React.createElement(
-    "div", 
-    {id:"parent"},
-    React.createElement("div", {id:"child"}, [
-        React.createElement("h1", {}, "Im an h1 tag"),
-        React.createElement("h2",{id:"child2"}, "I am h2 tag") 
-    ]));
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-console.log(parent);
+//React Element => Is an object which when rendered onto DOM it becomes a HTML Element
+
+//JSX is transpiled by Parcel before it reached the JS engine helped by Babel. Babel transpiled the JSX code into browser compatible/JS understandable code => React.createElement
+
+const number = 1000;
+
+//React Element
+const heading = (
+    <h1 id="heading" className="head">
+        Namaste React using JSX !!
+    </h1>
+);
+
+//React Title Component
+const Title = ()=>(
+    <h1>
+        This is the title component
+    </h1>
+)
+
+//React Functional Component with component composition(title inside)
+const HeadingComponent = () =>(
+    <div id="container">
+        <h2>{number}</h2>
+        {heading}
+        <Title/> 
+        <h1 className="heading">
+            Namaste React Functional Component
+        </h1>
+    </div>
+    
+)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-console.log(root)
-
-root.render(parent);
+root.render(<HeadingComponent/>);
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Namaste React 
+
+...
+# Parcel
+- Dev build
+- Local Server
+- HMR - Hot Module Replacement 
+- File watching algorith written in C++
+- Caching
+- Image Optimization
+- Minification 
+- Bundling
+- Compress 
+- Consistent Hashing
+- Code Splitting 
+- Differential Bundling - to handle opening of app in different browsers(older too) and environments
+- HTTPS environments supported too not just local hosts (for testing too)
+- Tree Shaking - remove unused code 
+- Different dev and prod builds
+
+When you do npx parcel build index.html - error because in pacakage.json you kept "main": App.js which is not required. So remove that 
+
+
+
+
+
+
+
